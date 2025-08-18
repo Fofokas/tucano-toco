51cf078b89def224af0c16d262ac5e3b59e45032
Murilo Luis Iossi Lopes <murilo.lopes32@etec.sp.gov.br>
2025-08-18 11:02:07 -0300

    tucao

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 //import './App.css'
+import tucano from './assets/tucano.png'
+
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
 
 import Header from './components/Header'
@@ -12,7 +14,11 @@ function App() {
     <>
       <Header />
 
-      
+      <h1>
+        O tuco é legal
+      </h1>
+
+      <img src={tucano} alt="" />
     </>
   )
 }
